8bf0d7ec826bb76785eb4d4e0401acdf136db701
Lordcreos <leonardosanchez4h@hotmail.com>
2021-04-27 14:36:25 -0500

    Agregar archivos de proyecto.

---
+++ Semana2lsan/Semana2lsan/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana2lsan
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+
+
+        private async void btnlogin_Clicked(object sender, EventArgs e)
+        {
+            try {
+                String usuario = Convert.ToString(user.Text);
+                String password = Convert.ToString(pass.Text);
+                if (usuario == "estudiante2021" && password == "uisrael2021")
+                {
+                    await Navigation.PushAsync(new login());
+                }
+                else
+                {
+                    DisplayAlert("Alerta", "Inserte sus credenciales de forma correcta", "Gracias");
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Gracias");
+            }
+        }
+    }
+}
---
+++ Semana2lsan/Semana2lsan/login.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana2lsan
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class login : ContentPage
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTotal_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double dato1 = Convert.ToDouble(TxtDato1.Text);
+                double dato2 = Convert.ToDouble(TxtDato2.Text);
+                double dato3 = Convert.ToDouble(TxtDato3.Text);
+                double dato4 = Convert.ToDouble(TxtDato4.Text);
+
+                double total = ((dato1 * 0.3) + (dato2 * 0.2)) + ((dato3 * 0.3) + (dato4 * 0.2));
+
+                TxtTotal.Text = total.ToString();
+                if (total >= 7)
+                {
+                    DisplayAlert("Alerta", "Aprobado", "Gracias");
+                }
+                else
+                {
+                    DisplayAlert("Alerta", "Reprobado", "Gracias");
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Gracias");
+            }
+        }
+    }
+}
